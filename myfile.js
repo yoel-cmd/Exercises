5b1cf11cf0aebfec1e3a9199d1fb919bada1a951
yoel ider <yoeli5746@gmail.com>
2025-06-22 18:08:07 +0300

    commit first

---
+++ myfile.js
@@ -0,0 +1,144 @@
+
+// const age =29;
+// console.log(age)
+// console.log(typeof age)
+
+
+// const name =`yoel`;
+// console.log(`my name is ${name}`)
+// console.log(typeof name)
+
+
+// const address =`migdal Haemeq`
+// console.log(`address ==> ${address}`)
+// console.log(typeof address)
+
+
+// const vegan=false;
+// console.log(` are you vegan => ${vegan}`)
+// console.log(typeof vegan)
+
+// const num_of_kids=4;
+// console.log(` num of kids => ${num_of_kids}`)
+// console.log(typeof num_of_kids)
+
+// const res =true / 0;
+
+// res=5+`dd`;
+// console.log(res)
+
+
+// res=true-15;
+// console.log(res)
+
+// console.log(undefined * 2);
+
+// console.log(res / null)
+// console.log (5 < `ishfhsfghai`);
+
+
+
+// console.log(0 || false);
+
+
+// const num= 234;
+// if(num < 120){
+//     console.log(`not`)
+// }else if(num ===233){
+//     console.log(`ok`)
+// }else{
+//     console.log(`non`)
+// }
+
+// const msg = num === 234 ? `yes`:`no`;
+
+
+// const age =13;
+// const height =120;
+// const somOfManager=true;
+
+// if(somOfManager === true){
+//     console.log(`welcome`)
+// }else if(age < 15){
+//     console.log(`get out `)
+// }else if(age > 15 && hige < 110){
+//     console.log(`go to kids ride`)          
+// }else if ({
+//     console.log(` go to on rollercoaster`)
+// }
+
+
+// let num= 0;
+// while(num < 101){
+
+//     console.log(num)
+//     num++;
+// }
+
+// for(let i=0;i<=50;i+=3){
+//     console.log(i)
+// }
+
+// const num= 1234;
+
+// let sum= num+"";
+// let zz =" ";
+// for(let i =sum.length;i>=0;i--)
+//     zz+=sum[i];
+// console.log(typeof zz)
+
+// const array =[];
+
+// array.unshift(1);
+
+// array.push(10);
+// array.splice(1,0,true,`q`);
+// for(let i=0;i<array.length;i++){
+//     console.log(array[i]);
+// }
+// array.shift();
+
+// array.pop();
+
+// const key ='kese'
+
+
+
+// const person={};
+// person[key]=`yoel`;
+// console.log(person);
+
+
+
+// const num=array.shift();
+// const num2=array.pop();
+
+// // console.log(num);
+// const bool=array.splice(1,1);
+
+// array.unshift("zz");
+
+
+
+const person={}
+person.name=`yoel`;
+person.age=29;
+person.hobbies=['gaming','food','cooking'];
+person.favFood=['chiken','meet','rise'];
+console.log(person.name,person.age);
+
+let hobbie='';
+for(let x of person.hobbies){
+   hobbie += x+" ";
+}
+console.log(hobbie);
+
+let favFo='';
+for(let y of person.favFood){
+   favFo+=y+" ";
+}
+console.log(favFo);
+
+
+
+
