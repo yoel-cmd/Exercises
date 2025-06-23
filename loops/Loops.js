6482a89d70b8177dc7e62de99f57d4c1d150edff
yoel ider <yoeli5746@gmail.com>
2025-06-23 13:23:22 +0300

    conflict-Loops.js

--- loops/Loops.js
+++ loops/Loops.js
@@ -1,13 +1,12 @@
 //------------ex1--------------
+const arr1=[2,3,4,5,6,7,8,9,10]
 
- const arr1=[2,3,4,5,6,7,8,9,10]
+function doubleValues(num){
+    return num*2;
+}
 
- function doubleValues(num){
-     return num*2;
- }
-
- const res1=arr1.map(doubleValues);
- console.log(res1);
+const res1=arr1.map(doubleValues);
+console.log(res1);
 
 //------------ex2--------------
 const arr2=[2,3,4,5,6,7,8,9,10]
@@ -20,4 +19,6 @@ function onlyEvenValues(num){
 const res2=[];
 arr2.map(onlyEvenValues);
 console.log(res2);
+
+//------------ex3----------------
 
