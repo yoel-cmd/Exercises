f3f73c49af18eb1025b91c14413a81f512c1e0f5
yoel ider <yoeli5746@gmail.com>
2025-06-23 12:19:01 +0300

    loops ex2

---
+++ loops/Loops.js
@@ -0,0 +1,21 @@
+//------------ex1--------------
+// const arr=[2,3,4,5,6,7,8,9,10]
+
+// function doubleValues(num){
+//     return num*2;
+// }
+
+// const res=arr.map(doubleValues);
+// console.log(res);
+
+//------------ex2--------------
+const arr=[2,3,4,5,6,7,8,9,10]
+
+function onlyEvenValues(num){
+        if(num%2===0)
+            res.push(num) ;
+        
+}
+const res=[];
+arr.map(onlyEvenValues);
+console.log(res);
