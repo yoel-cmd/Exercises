32d8ee5ce6532d2749ee52b450084ebe1818ddde
yoel ider <yoeli5746@gmail.com>
2025-06-23 17:31:55 +0300

    loops ex3

--- loops/Loops.js
+++ loops/Loops.js
@@ -1,24 +1,38 @@
-//------------ex1--------------
-const arr1=[2,3,4,5,6,7,8,9,10]
+// //------------ex1--------------
+// const arr1=[2,3,4,5,6,7,8,9,10];
 
-function doubleValues(num){
-    return num*2;
-}
+// // function doubleValues(num){
+// //     return num*2;
+// // }
 
-const res1=arr1.map(doubleValues);
-console.log(res1);
+// const res1=arr1.map((num)=>{return num*2});
+// console.log(res1);
 
-//------------ex2--------------
-const arr2=[2,3,4,5,6,7,8,9,10]
+// //------------ex2--------------
+// const arr2=[2,3,4,5,6,7,8,9,10]
 
-function onlyEvenValues(num){
-        if(num%2===0)
-            res2.push(num) ;
+// function onlyEvenValues(num){
+//         if(num%2===0)
+//             res2.push(num) ;
         
-}
-const res2=[];
-arr2.map(onlyEvenValues);
-console.log(res2);
+// }
+// const res2=[];
+// arr2.map(onlyEvenValues);
+// console.log(res2);
 
 //------------ex3----------------
 
+const mixedArray = ["first", 42, true, "middle", null, "last"];
+
+    let newArr=[];
+
+function showFirstAndLast(arr){
+    if(typeof arr[0]===`string`){
+    newArr.push(arr.shift())
+    }
+    if(typeof arr[arr.length-1]===`string`){
+    newArr.push(arr.pop())
+    }
+    return newArr;
+}
+console.log(showFirstAndLast(mixedArray));
